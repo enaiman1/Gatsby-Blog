71093044e4685bce63b1beefd8f8ccc477222eee
Ericcentrik <enaiman1@gmail.com>
2020-10-27 09:34:14 -0400

    added some style to header and nav bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react"
 
-import Header from './header'
-import Footer from './footer'
-import '../styles/index.scss'
+import Header from "./header"
+import Footer from "./footer"
+import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
-const Layout = (props) => {
-    return (
-<div>
-    <Header />
-{props.children}
-<Footer />
-</div>
-    )
+const Layout = props => {
+  return (
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
+
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
