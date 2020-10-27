02942bdf2f6ebfd1f3be5143cfc2507818253a10
Ericcentrik <enaiman1@gmail.com>
2020-10-26 21:57:20 -0400

    created blog, about, and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl
+        eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a
+        bibendum metus. Donec scelerisque solli
+      </p>
+      <p>
+        conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut
+        vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar
+        vitae dolor. Integer eu nibh at nisi ullamcorper sagittis id vel leo.
+        Integer feugiat faucibus libero
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+   
+    <div>
+      <h1>Contact Me</h1>
+   <p> The best way to reach me is via @ericcentrik on Instagram!</p>
+    </div>
+  
+  )
+}
+
+export default ContactPage
