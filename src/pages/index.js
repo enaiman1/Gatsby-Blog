85164e37d1205a5ebbef9e840c4ddf3795df7b18
Ericcentrik <enaiman1@gmail.com>
2020-10-27 16:58:42 -0400

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,7 @@ const IndexPage = () => {
       <Head title="Home" />
        <h1>Hello.</h1>
       <h2>I'm Eric, a full stack developer living in beautiful Orlando</h2>
-      
-      <p>Need a developer? <Link to="/contact"> Contact me.</Link></p>
+  
     </Layout>
   )
 
